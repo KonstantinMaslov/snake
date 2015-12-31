9720b128a93f299dcd5a6b948efc79f93d4e3c21
Maslov <stalinvserdce@gmail.com>
2015-12-31 15:55:23 +0200

    Проверка обратной связи

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+            Console.Write("Hello World!!!");
             Console.Read();
         }
     }
